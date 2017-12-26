cc30d6fc75272a36a43e4585937464c271b3338c
sikandre <sik.andre@gmail.com>
2017-12-26 23:10:41 +0000

    ja aparecem os quadrados mas mal formatados

--- java/pt/poo/isel/squares/View/GoalView.java
+++ java/pt/poo/isel/squares/View/GoalView.java
@@ -16,20 +16,38 @@ import pt.poo.isel.squares.model.square.Square;
 import pt.poo.isel.tile.Tile;
 import pt.poo.isel.tile.TileView;
 
-public class GoalView extends LinearLayout {
-    SquareView square;
+public class GoalView extends LinearLayout implements Tile {
+    TileView square;
     TextView remainGoal;
     Paint paint= new Paint();
+    Square s;
 
     public GoalView(Context context, Squares.Goal g) {
         super(context);
+        s=g.square;
         remainGoal=new TextView(getContext());
+        TileView square = new TileView(getContext());
+        square.setTile(SquareView.newInstance(s));
         //square=SquareView.newInstance(g.square);
         remainGoal.setTextSize(20);
         remainGoal.setBackgroundColor(Color.BLUE);
         remainGoal.setText(" "+g.number+" ");
         addView(remainGoal);
+        addView(square);
         //SquareView.newInstance(g.square);
+    }
+
+    @Override
+    public void draw(@NonNull Canvas canvas, int side) {
+        int color = s.getColor();
+        paint.setColor(color);
+        RectF r = new RectF(0, 0, side, side);
+        canvas.drawRoundRect(r,32,32, paint);
+    }
+
+    @Override
+    public boolean setSelect(boolean selected) {
+        return false;
     }
 
 /*
